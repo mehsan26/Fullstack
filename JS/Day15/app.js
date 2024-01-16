04ce1570cb7f0646179f6c8227adf0868176b4f2
mehsan26 <mehsanfahad@gmail.com>
2024-01-16 22:36:04 +0530

    learned about console.log

---
+++ JS/Day15/app.js
@@ -0,0 +1,11 @@
+let pencitPrice = 10;
+let eraserPrice = 5;
+let shepnerPrice = 5;
+let output =
+  "Total Price is : " + (pencitPrice + eraserPrice + shepnerPrice) + "rupees";
+let output2 = `Total Price is : ${
+  pencitPrice + eraserPrice + shepnerPrice
+} rupees output 2`;
+console.log("Hi, I am Mehsan.");
+console.log(output2);
+console.log(output);
